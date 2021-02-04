0bf181982dc51b920d3fe581d734ad74b056b4b2
Phuc Le <phuclh93@gmail.com>
2021-02-03 18:15:58 -0800

    Move Rate model + factory + migration

--- config/escape-room.php
+++ config/escape-room.php
@@ -2,4 +2,18 @@
 
 return [
 
+    'model_class' => [
+
+        'user' => \App\Models\User::class,
+
+        'room' => \App\Models\Room::class,
+
+        'slot' => \App\Models\Slot::class,
+
+        'booking' => \App\Models\Booking::class,
+
+        'schedule' => \App\Models\Schedule::class,
+
+    ]
+
 ];
