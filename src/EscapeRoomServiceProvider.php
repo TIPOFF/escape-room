f7c46026debed8522fc6614d9cff8a9da74e268a
Beau Beauchamp, WebTigers <beau@webtigers.com>
2021-02-09 17:48:20 -0500

    Load Migrations from Package

--- src/EscapeRoomServiceProvider.php
+++ src/EscapeRoomServiceProvider.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tipoff\EscapeRoom;
 
 use Illuminate\Support\Str;
@@ -9,6 +11,12 @@ use Tipoff\EscapeRoom\Commands\EscapeRoomCommand;
 
 class EscapeRoomServiceProvider extends PackageServiceProvider
 {
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        parent::boot();
+    }
+
     public function configurePackage(Package $package): void
     {
         /*
@@ -18,30 +26,6 @@ class EscapeRoomServiceProvider extends PackageServiceProvider
          */
         $package
             ->name('escape-room')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigrations([
-                '2020_02_16_100000_create_rates_table',
-                '2020_02_16_130000_create_supervisions_table',
-                '2020_02_18_100000_create_themes_table',
-                '2020_02_18_110000_create_rooms_table',
-                '2020_05_11_100000_create_participants_table',
-            ])
-            ->hasCommand(EscapeRoomCommand::class);
-    }
-
-    /**
-     * Using packageBooted lifecycle hooks to override the migration file name.
-     * We want to keep the old filename for now.
-     */
-    public function packageBooted()
-    {
-        foreach ($this->package->migrationFileNames as $migrationFileName) {
-            if (! $this->migrationFileExists($migrationFileName)) {
-                $this->publishes([
-                    $this->package->basePath("/../database/migrations/{$migrationFileName}.php.stub") => database_path('migrations/' . Str::finish($migrationFileName, '.php')),
-                ], "{$this->package->name}-migrations");
-            }
-        }
+            ->hasConfigFile();
     }
 }
