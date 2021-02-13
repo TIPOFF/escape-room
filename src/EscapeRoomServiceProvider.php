9d193180e1cb3c00496939a46d088bd07499307f
Drew Roberts <github@drewroberts.com>
2021-02-13 10:17:00 -0500

    Remove policies since not created yet

--- src/EscapeRoomServiceProvider.php
+++ src/EscapeRoomServiceProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Tipoff\EscapeRoom;
 
-use Tipoff\EscapeRoom\Policies\EscapeRoomPolicy;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
 
@@ -13,9 +12,6 @@ class EscapeRoomServiceProvider extends TipoffServiceProvider
     public function configureTipoffPackage(TipoffPackage $package): void
     {
         $package
-            ->hasPolicies([
-                EscapeRoom::class => EscapeRoomPolicy::class,
-            ])
             ->name('escape-room')
             ->hasConfigFile();
     }
