6f532fff6f4de5227d7fa68ec2e2048cd3c4b3d0
devjk1 <jinsik@omniadigital.io>
2021-02-13 13:47:08 -0500

    [TGER-47] Added policies, permissions, getPackageProviders TestCase

--- src/EscapeRoomServiceProvider.php
+++ src/EscapeRoomServiceProvider.php
@@ -4,8 +4,19 @@ declare(strict_types=1);
 
 namespace Tipoff\EscapeRoom;
 
+use Illuminate\Support\Facades\Gate;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Tipoff\EscapeRoom\Models\Participant;
+use Tipoff\EscapeRoom\Models\Rate;
+use Tipoff\EscapeRoom\Models\Room;
+use Tipoff\EscapeRoom\Models\Supervision;
+use Tipoff\EscapeRoom\Models\Theme;
+use Tipoff\EscapeRoom\Policies\ParticipantPolicy;
+use Tipoff\EscapeRoom\Policies\RatePolicy;
+use Tipoff\EscapeRoom\Policies\RoomPolicy;
+use Tipoff\EscapeRoom\Policies\SupervisionPolicy;
+use Tipoff\EscapeRoom\Policies\ThemePolicy;
 
 class EscapeRoomServiceProvider extends PackageServiceProvider
 {
@@ -18,13 +29,17 @@ class EscapeRoomServiceProvider extends PackageServiceProvider
 
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('escape-room')
             ->hasConfigFile();
+    }
+
+    public function registeringPackage()
+    {
+        Gate::policy(Participant::class, ParticipantPolicy::class);
+        Gate::policy(Rate::class, RatePolicy::class);
+        Gate::policy(Room::class, RoomPolicy::class);
+        Gate::policy(Supervision::class, SupervisionPolicy::class);
+        Gate::policy(Theme::class, ThemePolicy::class);
     }
 }
