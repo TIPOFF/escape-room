4a7157be19eeededfbfdbc0577c28ecd866923bc
arwaawan <awan.arwa@aerocareusa.com>
2021-02-17 14:47:25 -0600

    [TGER-68] - migrate nova filters

---
+++ src/Nova/Filters/Room.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\Bookings\Nova\Filters;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Filters\Filter;
+
+class Room extends Filter
+{
+    /**
+     * The filter's component.
+     *
+     * @var string
+     */
+    public $component = 'select-filter';
+
+    /**
+     * Apply the filter to the given query.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  mixed  $value
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function apply(Request $request, $query, $value)
+    {
+        return $query->where('room_id', $value);
+    }
+
+    /**
+     * Get the filter's available options.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function options(Request $request)
+    {
+        $models = \Tipoff\EscapeRoom\Models\Room::all();
+
+        return $models->pluck('id', 'name')->all();
+    }
+}
