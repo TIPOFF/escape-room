f5b549191b40e4ca15ddbd61c8e482a17a62aaa0
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-02-20 18:48:57 -0500

    remove participant from the escapeRommServiceProvider

--- src/EscapeRoomServiceProvider.php
+++ src/EscapeRoomServiceProvider.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace Tipoff\EscapeRoom;
 
-use Tipoff\EscapeRoom\Models\Participant;
 use Tipoff\EscapeRoom\Models\Rate;
 use Tipoff\EscapeRoom\Models\Room;
 use Tipoff\EscapeRoom\Models\Supervision;
 use Tipoff\EscapeRoom\Models\Theme;
-use Tipoff\EscapeRoom\Policies\ParticipantPolicy;
 use Tipoff\EscapeRoom\Policies\RatePolicy;
 use Tipoff\EscapeRoom\Policies\RoomPolicy;
 use Tipoff\EscapeRoom\Policies\SupervisionPolicy;
@@ -23,14 +21,12 @@ class EscapeRoomServiceProvider extends TipoffServiceProvider
     {
         $package
             ->hasPolicies([
-                Participant::class => ParticipantPolicy::class,
                 Rate::class => RatePolicy::class,
                 Room::class => RoomPolicy::class,
                 Supervision::class => SupervisionPolicy::class,
                 Theme::class => ThemePolicy::class,
             ])
             ->hasNovaResources([
-                \Tipoff\EscapeRoom\Nova\Participant::class,
                 \Tipoff\EscapeRoom\Nova\Rate::class,
                 \Tipoff\EscapeRoom\Nova\Room::class,
                 \Tipoff\EscapeRoom\Nova\Supervision::class,
