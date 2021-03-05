427bc39271c0b57a328f3ee45877cfb175bdd991
wolfrednicolas <wolfrednicolas@users.noreply.github.com>
2021-03-05 17:04:32 +0000

    Fix styling

--- tests/Support/Providers/NovaPackageServiceProvider.php
+++ tests/Support/Providers/NovaPackageServiceProvider.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 namespace Tipoff\EscapeRoom\Tests\Support\Providers;
 
-use Tipoff\TestSupport\Providers\BaseNovaPackageServiceProvider;
 use Tipoff\EscapeRoom\Nova\EscapeRoomLocation;
 use Tipoff\EscapeRoom\Nova\EscapeRoomMarket;
 use Tipoff\EscapeRoom\Nova\Rate;
 use Tipoff\EscapeRoom\Nova\Room;
 use Tipoff\EscapeRoom\Nova\Supervision;
 use Tipoff\EscapeRoom\Nova\Theme;
+use Tipoff\TestSupport\Providers\BaseNovaPackageServiceProvider;
 
 class NovaPackageServiceProvider extends BaseNovaPackageServiceProvider
 {
-	public static array $packageResources = [
+    public static array $packageResources = [
         EscapeRoomLocation::class,
         EscapeRoomMarket::class,
         Rate::class,
