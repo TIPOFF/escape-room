24dd87f8a468d0f9cedeadb598cba8edf3e53a3e
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-03-05 12:04:03 -0500

    improve code

--- tests/Support/Providers/NovaPackageServiceProvider.php
+++ tests/Support/Providers/NovaPackageServiceProvider.php
@@ -5,7 +5,21 @@ declare(strict_types=1);
 namespace Tipoff\EscapeRoom\Tests\Support\Providers;
 
 use Tipoff\TestSupport\Providers\BaseNovaPackageServiceProvider;
+use Tipoff\EscapeRoom\Nova\EscapeRoomLocation;
+use Tipoff\EscapeRoom\Nova\EscapeRoomMarket;
+use Tipoff\EscapeRoom\Nova\Rate;
+use Tipoff\EscapeRoom\Nova\Room;
+use Tipoff\EscapeRoom\Nova\Supervision;
+use Tipoff\EscapeRoom\Nova\Theme;
 
 class NovaPackageServiceProvider extends BaseNovaPackageServiceProvider
 {
+	public static array $packageResources = [
+        EscapeRoomLocation::class,
+        EscapeRoomMarket::class,
+        Rate::class,
+        Room::class,
+        Supervision::class,
+        Theme::class,
+    ];
 }
