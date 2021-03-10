e0cf0a35fdf85ffe4067d8b771a03c109501504f
arwaawan <awan.arwa@aerocareusa.com>
2021-03-09 21:33:44 -0600

    add user roles to permissions migration

--- database/migrations/2021_02_13_100000_add_escape_room_permissions.php
+++ database/migrations/2021_02_13_100000_add_escape_room_permissions.php
@@ -10,24 +10,24 @@ class AddEscapeRoomPermissions extends BasePermissionsMigration
     {
 
         $permissions = [
-             'view rates',
-             'create rates',
-             'update rates',
-             'view supervisions',
-             'create supervisions',
-             'update supervisions',
-             'view themes',
-             'create themes',
-             'update themes',
-             'view rooms',
-             'create rooms',
-             'update rooms',
-             'view escape room locations',
-             'create escape room locations',
-             'update escape room locations',
-             'view escape room markets',
-             'create escape room markets',
-             'update escape room markets'
+             'view rates' => ['Owner', 'Staff'],
+             'create rates' => ['Owner'],
+             'update rates' => ['Owner'],
+             'view supervisions' => ['Owner', 'Staff'],
+             'create supervisions' => ['Owner'],
+             'update supervisions' => ['Owner'],
+             'view themes' => ['Owner', 'Staff'],
+             'create themes' => ['Owner'],
+             'update themes' => ['Owner'],
+             'view rooms' => ['Owner', 'Staff'],
+             'create rooms' => ['Owner'],
+             'update rooms' => ['Owner'],
+             'view escape room locations' => ['Owner', 'Staff'],
+             'create escape room locations' => ['Owner'],
+             'update escape room locations' => ['Owner'],
+             'view escape room markets' => ['Owner', 'Staff'],
+             'create escape room markets' => ['Owner'],
+             'update escape room markets' => ['Owner']
         ];
 
         $this->createPermissions($permissions);
