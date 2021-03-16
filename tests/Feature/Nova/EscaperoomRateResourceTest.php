bfcbdab4982acf2b780542237f931baa763a8a45
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-03-16 10:26:35 -0400

    add nova test without camel case

---
+++ tests/Feature/Nova/EscaperoomRateResourceTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\EscapeRoom\Tests\Feature\Nova;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tipoff\EscapeRoom\Models\EscaperoomRate;
+use Tipoff\EscapeRoom\Tests\TestCase;
+
+class EscaperoomRateResourceTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @test */
+    public function index()
+    {
+        EscaperoomRate::factory()->count(1)->create();
+
+        $this->actingAs(self::createPermissionedUser('view rates', true));
+
+        $response = $this->getJson('nova-api/rates')->assertOk();
+
+        $this->assertCount(1, $response->json('resources'));
+    }
+}
