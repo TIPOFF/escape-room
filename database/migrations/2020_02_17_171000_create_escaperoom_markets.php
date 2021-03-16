5b4f4c619633c10f8d846c90ae326a4456309412
Drew Roberts <github@drewroberts.com>
2021-03-16 13:04:48 -0400

    Add market relationship to escape room market

--- database/migrations/2020_02_17_171000_create_escaperoom_markets.php
+++ database/migrations/2020_02_17_171000_create_escaperoom_markets.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Tipoff\Locations\Models\Location;
 
 class CreateEscaperoomMarkets extends Migration
 {
@@ -13,14 +12,13 @@ class CreateEscaperoomMarkets extends Migration
     {
         Schema::create('escaperoom_markets', function (Blueprint $table) {
             $table->id();
-
+            $table->foreignIdFor(app('market'))->unique();	// NOTE - unique() added -- there should be exactly one record per market
             $table->boolean('corporate')->default(true);
 
             $table->text('rooms_content')->nullable(); // Market specific content for /escape-rooms page. Written in Markdown.
             $table->text('faq_content')->nullable(); // Frequently Asked Questions about the market (such as where to park at each location in the market). Written in Markdown.
             $table->text('competitors_content')->nullable(); // Nice paragraph about the other escape rooms in each market. First used on /escape-rooms page. Written in Markdown.
-
-            $table->foreignIdFor(Location::class)->unique();	// NOTE - unique() added -- there should be exactly one record per location!
+            
             $table->foreignIdFor(app('user'), 'creator_id');
             $table->foreignIdFor(app('user'), 'updater_id');
 
