00cd481682c3604b457e2a4a26ed7c35dd2835e3
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-03-16 10:23:40 -0400

    add models without camel case

---
+++ src/Models/EscaperoomLocation.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\EscapeRoom\Models;
+
+use DrewRoberts\Media\Models\Image;
+use Tipoff\Locations\Models\Location;
+use Tipoff\Support\Models\BaseModel;
+use Tipoff\Support\Traits\HasCreator;
+use Tipoff\Support\Traits\HasPackageFactory;
+use Tipoff\Support\Traits\HasUpdater;
+
+class EscaperoomLocation extends BaseModel
+{
+    use HasCreator;
+    use HasUpdater;
+    use HasPackageFactory;
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function teamPhoto()
+    {
+        return $this->belongsTo(Image::class, 'team_image_id');
+    }
+}
