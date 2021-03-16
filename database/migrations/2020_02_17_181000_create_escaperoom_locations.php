d0d95d232a6e31f6c3db2317d53c322cf83a921e
Drew Roberts <github@drewroberts.com>
2021-03-16 13:07:05 -0400

    Update migration for escape room location

--- database/migrations/2020_02_17_181000_create_escaperoom_locations.php
+++ database/migrations/2020_02_17_181000_create_escaperoom_locations.php
@@ -6,7 +6,6 @@ use DrewRoberts\Media\Models\Image;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Tipoff\Locations\Models\Location;
 
 class CreateEscaperoomLocations extends Migration
 {
@@ -14,14 +13,17 @@ class CreateEscaperoomLocations extends Migration
     {
         Schema::create('escaperoom_locations', function (Blueprint $table) {
             $table->id();
-            $table->boolean('corporate')->default(true); // Mark false for Miami & DC
+            $table->foreignIdFor(app('location'))->unique();	// NOTE - unique() added -- there should be exactly one record per location!
+            $table->boolean('corporate')->default(true);
+            $table->boolean('covid')->default(false); // Mark true if location closed due to COVID-19
+            
             $table->string('team_names')->nullable();
             $table->foreignIdFor(app('image'), 'team_image_id')->nullable();
+            
             $table->unsignedTinyInteger('booking_cutoff'); // Minutes before a game/slot to cutoff the booking window.
-            $table->boolean('covid')->default(false); // Mark true if location closed due to COVID-19
             $table->boolean('use_iframe')->default(false); // If yes, use the booking iframe below
-            $table->text('booking_iframe')->nullable(); // Iframe code for Resova/Bookeo or other 3rd parting booking software
-            $table->foreignIdFor(Location::class)->unique();	// NOTE - unique() added -- there should be exactly one record per location!
+            $table->text('booking_iframe')->nullable(); // Iframe code for 3rd parting booking software
+            
             $table->foreignIdFor(app('user'), 'creator_id');
             $table->foreignIdFor(app('user'), 'updater_id');
             $table->timestamps();
